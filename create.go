c89667bfd494b8e0f19c7098f0e80c16d300780e
Andrew Fields <andybug10@gmail.com>
2016-02-27 10:28:58 -0600

    Use built-in logging

--- create.go
+++ create.go
@@ -1,7 +1,9 @@
 package main
 
+import "log"
+
 func createDatabase(root string, outPath string) error {
-	log.Notice("Creating database at root " + root)
-	log.Info("Outputing to " + outPath)
+	log.Println("Creating database with root at " + root)
+	log.Println("Outputing to " + outPath)
 	return nil
 }
--- spredendb.go
+++ spredendb.go
@@ -3,40 +3,25 @@ package main
 import (
 	"os"
 	"fmt"
-	"github.com/op/go-logging"
+	"log"
 )
-
-// logging setup
-var log = logging.MustGetLogger("logger")
 
 const usage string = "usage: spredendb <command>"
 
 
-func initLogging() {
-	var format = logging.MustStringFormatter(
-		`%{color}%{time:15:04:05.000} %{shortfunc}: %{level:.4s} %{id:03x}%{color:reset} %{message}`,
-	)
-
-	backend := logging.NewLogBackend(os.Stdout, "", 0)
-	backendFormatter := logging.NewBackendFormatter(backend, format)
-	backendLeveled := logging.AddModuleLevel(backend)
-	backendLeveled.SetLevel(logging.ERROR, "")
-	logging.SetBackend(backendLeveled, backendFormatter)
-}
-
 func main() {
+	log.SetFlags(log.Lmicroseconds | log.Lshortfile)
+
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, usage)
 		os.Exit(1)
 	}
 
-	initLogging()
-
 	switch os.Args[1] {
 	case "create":
 		err := createDatabase(os.Args[2], os.Args[3])
 		if err != nil {
-			log.Critical(err)
+			log.Fatal(err)
 		}
 	case "tree":
 		fmt.Println("tree command")
