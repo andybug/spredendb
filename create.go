95b54b756a94ab30c34cd9cd66214e7940cc7329
Andrew Fields <andybug10@gmail.com>
2016-02-27 21:00:42 -0600

    List sports from data root

--- create.go
+++ create.go
@@ -1,9 +1,35 @@
 package main
 
-import "log"
+import (
+	"log"
+	"io/ioutil"
+)
+
+type Sport struct {
+	name string
+	path string
+}
+
+func listSports(root string) (sports []string) {
+	files, err := ioutil.ReadDir(root)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		log.Println("file " + f.Name())
+		if f.IsDir() {
+			sports = append(sports, f.Name())
+		}
+	}
+
+	return sports
+}
 
 func createDatabase(root string, outPath string) error {
 	log.Println("Creating database with root at " + root)
 	log.Println("Outputing to " + outPath)
+	sports := listSports(root)
+
 	return nil
 }
