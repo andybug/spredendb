5e0c14f3da52ce2400a6d452bd26bd37880d2793
Andrew Fields <andybug10@gmail.com>
2016-03-08 21:27:29 -0600

    Reimplement create.go

--- create.go
+++ create.go
@@ -3,39 +3,64 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"path"
 	"regexp"
 	"strconv"
+	//	"errors"
 )
 
+type Team struct {
+	uuid string
+	name string
+}
+
+type Game struct {
+	date       string
+	uuid       string
+	home_uuid  string
+	home_score int
+	away_uuid  string
+	away_score int
+}
+
+type Round struct {
+	path  string
+	games []*Game
+}
+
 type Season struct {
-	year int
+	path   string
+	year   int
+	teams  []*Team
+	rounds []*Round
 }
 
 type Sport struct {
-	name    string
 	path    string
+	name    string
 	seasons []*Season
 }
 
-func listSports(root string) (sports []*Sport) {
-	files, err := ioutil.ReadDir(root)
+func (season *Season) readRounds() error {
+	files, err := ioutil.ReadDir(season.path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, f := range files {
-		if f.IsDir() {
-			sport := new(Sport)
-			sport.name = f.Name()
-			sport.path = root + "/" + f.Name()
-			sports = append(sports, sport)
+		match, _ := regexp.MatchString("^round\\d{2}.json$", f.Name())
+		if match {
+			round := new(Round)
+			round.path = season.path + "/" + f.Name()
+			//round.games = readGames(round)
+			season.rounds = append(season.rounds, round)
 		}
 	}
 
-	return sports
+	return nil
 }
 
-func listSeasons(sport *Sport) (seasons []*Season) {
+func (sport *Sport) readSeasons() {
 	files, err := ioutil.ReadDir(sport.path)
 	if err != nil {
 		log.Fatal(err)
@@ -45,25 +70,22 @@ func listSeasons(sport *Sport) (seasons []*Season) {
 		match, _ := regexp.MatchString("^\\d{4}$", f.Name())
 		if f.IsDir() && match {
 			season := new(Season)
+			season.path = sport.path + "/" + f.Name()
 			season.year, _ = strconv.Atoi(f.Name())
+			season.readRounds()
+			sport.seasons = append(sport.seasons, season)
 		}
 	}
-
-	return nil
 }
 
 func createDatabase(root string, outPath string) error {
 	log.Println("Creating database with root at " + root)
 	log.Println("Outputing to " + outPath)
-	sports := listSports(root)
 
-	for _, sport := range sports {
-		log.Println("Processing sport " + sport.name)
-		sport.seasons = listSeasons(sport)
-		for _, season := range sport.seasons {
-			log.Printf("Processing year %d\n", season.year)
-		}
-	}
+	sport := new(Sport)
+	sport.path = root
+	sport.name = path.Base(root)
+	sport.readSeasons()
 
 	return nil
 }
